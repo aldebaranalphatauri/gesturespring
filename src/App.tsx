90db99fffb20e3b39eee05f72c08f97c9a27d3a9
Angelo Rimoldi <angeloerimoldi@gmail.com>
2022-09-21 13:27:30 +0200

    Horizontal

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ import swap from 'lodash-move'
 import styles from './styles.module.css'
 
 const fn =
-  (order: number[], active = false, originalIndex = 0, curIndex = 0, y = 0) =>
+  (order: number[], active = false, originalIndex = 0, curIndex = 0, x = 0) =>
   (index: number) =>
     active && index === originalIndex
       ? {
-          y: curIndex * 100 + y,
+          x: curIndex * 100 + x,
           scale: 1.1,
           zIndex: 1,
           shadow: 15,
           immediate: (key: string) => key === 'zIndex',
-          config: (key: string) => (key === 'y' ? config.stiff : config.default),
+          config: (key: string) => (key === 'x' ? config.stiff : config.default),
         }
       : {
-          y: order.indexOf(index) * 100,
+          x: order.indexOf(index) * 100,
           scale: 1,
           zIndex: 0,
           shadow: 1,
@@ -29,24 +29,24 @@ const fn =
 function DraggableList({ items }: { items: string[] }) {
   const order = useRef(items.map((_, index) => index)) // Store indicies as a local ref, this represents the item order
   const [springs, api] = useSprings(items.length, fn(order.current)) // Create springs, each corresponds to an item, controlling its transform, scale, etc.
-  const bind = useDrag(({ args: [originalIndex], active, movement: [, y] }) => {
+  const bind = useDrag(({ args: [originalIndex], active, movement: [x] }) => {
     const curIndex = order.current.indexOf(originalIndex)
-    const curRow = clamp(Math.round((curIndex * 100 + y) / 100), 0, items.length - 1)
+    const curRow = clamp(Math.round((curIndex * 100 + x) / 100), 0, items.length - 1)
     const newOrder = swap(order.current, curIndex, curRow)
-    api.start(fn(newOrder, active, originalIndex, curIndex, y)) // Feed springs new style data, they'll animate the view without causing a single render
+    api.start(fn(newOrder, active, originalIndex, curIndex, x)) // Feed springs new style data, they'll animate the view without causing a single render
     if (!active) order.current = newOrder
   })
 
   return (
     <div className={styles.content} style={{ height: items.length * 100 }}>
-      {springs.map(({ zIndex, shadow, y, scale }, i) => (
+      {springs.map(({ zIndex, shadow, x, scale }, i) => (
         <animated.div
           {...bind(i)}
           key={i}
           style={{
             zIndex,
             boxShadow: shadow.to(s => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`),
-            y,
+            x,
             scale,
           }}
           children={items[i]}
